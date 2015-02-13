4b17e07058f58eeffa4e2c4e6e03207b1016e646
pbfrias17 <pbfrias17@gmail.com>
2015-02-13 13:28:38 -0500

    start bfs

--- proj1.py
+++ proj1.py
@@ -16,26 +16,31 @@ def main():
     matrix = createMatrix(masterList)
 
     #Breadth First Search
+    doBFS(0, 10, matrix)
     
             
 def createMatrix(_list):
     largest = 0
     CONST_OFFSET = 64
     
-    #create graph matrix
-    for i in _list:
-        if len(i) <= 1:
-            if ord(i) > 64 and ord(i) < 90:
-                i = ord(i) - CONST_OFFSET
-                if largest < i:
-                    largest = i
+    #create empty graph matrix of appropriate girth
+    for idx, val in enumerate(_list):
+        if len(_list[idx]) <= 1:
+            if ord(_list[idx]) > 64 and ord(_list[idx]) < 90:
+                _list[idx] = ord(_list[idx]) - CONST_OFFSET - 1
+                if largest < _list[idx]:
+                    largest = _list[idx]
 
-    print(largest)
-    matrix = [[0 for x in range(largest)] for y in range(largest)]
+    matrix = [[0 for x in range(largest + 1)] for y in range(largest + 1)]
 
-    #fill matrix
+    #fill matrix by iterating through list in groups of three
     itr = 0
-    while itr < len(_list):
+    for x_i in range(0,len(_list),3):
+        y_i = x_i + 1
+        weight_i = y_i + 1
+        matrix[_list[x_i]][_list[y_i]] = _list[weight_i]
+
+
     printMatrix(matrix)
 
 
@@ -44,7 +49,9 @@ def createMatrix(_list):
 def printMatrix(matrix):
     for stuff in matrix:
         print(stuff)
-    
+
+def doBFS(start_node, end_node, matrix):
+    print("Starting BFS from " + str(start_node) + " to " + str(end_node))
 
     
 main()
