adb1777963126dbe63e9cda986d9af4e49db1203
pbfrias17 <pbfrias17@gmail.com>
2015-05-03 19:04:13 -0400

    finish up

--- proj3.py
+++ proj3.py
@@ -32,37 +32,49 @@ class DecisionTree:
             leaf = vector.pop(0)
             self.descend(self.tree, vector, leaf)
 
-        print(data_set)
-        print(self.training_set)
-
     def classify(self, data_set):
         return self.descend(self.tree, data_set, None)
 
 
 class NaiveBayes:
+    
     def train(self, training_set):
-        #what features of the vector should we consider?
-
+        self.training_set = training_set
         self.amount_of_ones = 0
         self.total = 0
         for vector in training_set:
             self.total += 1
             self.amount_of_ones += vector[0]
-        print(self.amount_of_ones)
 
         self.prob_one = self.amount_of_ones / self.total
 
         #We will consider the amount of 1(s) in each vector
-        
         self.ones_in_each = []
         for vector in training_set:
             ones = 0
             for i in range(1, len(vector)):
                 ones += vector[i]
             self.ones_in_each.append(ones)
+        
+    def classify(self, data_set):
+        ones = 0
+        for var in data_set:
+            ones += var
 
-        print(self.ones_in_each)
+        #P(data_set is 1 | data_set has x 1s)
+        total_ones = 0
+        total_amount = 0
+        for i in range(len(self.ones_in_each)):
+            if self.ones_in_each[i] == ones:
+                total_ones += self.training_set[i][0]
+                total_amount += 1
                 
+        prob = total_ones / total_amount
+        if prob >= .5:
+            return 1
+
+        return 0
+
                 
                 
     
@@ -79,7 +91,7 @@ class NearestNbr:
             if nearest_dist < 0 or dist < nearest_dist:
                 nearest_dist = dist
                 nearest_index = i
-
+                
         return self.neighbors[nearest_index][0]
 
     def distance(self, data_set, index):
@@ -101,17 +113,21 @@ def main():
         for var in line.split():
             numbered_list.append(ord(var) - 48)
         test_set.append(numbered_list)
-    print(test_set)
+
+    data_set_list = [[1, 1, 1], [1, 1, 0], [1, 0, 1], [0, 0, 1], [0, 1, 1], [1, 0, 1], [0, 0, 0]]
 
     print("NAIVE BAYES:")
     NB = NaiveBayes()
     NB.train(test_set)
+    for data_set in data_set_list:
+        print(data_set, "classified as", NB.classify(data_set))
     print()
 
     print("NEAREST NEIGHBOR")
     NN = NearestNbr()
     NN.train(test_set)
-    print("'000' classified as: ", NN.classify([0, 0, 0]))
+    for data_set in data_set_list:
+        print(data_set, "classified as", NN.classify(data_set))
     print()
 
     #for some reason, couldn't get my train method to pass in the test_set
@@ -119,7 +135,9 @@ def main():
     print("DECISION TREE")
     DT = DecisionTree()
     DT.train(test_set)
-    print("deciding... ", DT.classify([1, 0, 1]))
+    for data_set in data_set_list:
+        clone_set = data_set[:]
+        print(clone_set, "classified as", DT.classify(data_set))
 
     return 0
 
